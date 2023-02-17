c32099e22b5ecb51147789746207024dbba03ae2
Luceniente <124631319+Luceniente@users.noreply.github.com>
2023-02-17 10:26:39 +0000

    Update changeProperty.js

--- changeProperty.js
+++ changeProperty.js
@@ -1,7 +1,8 @@
 const codeloccol = {
-    name: 'Happy Coder',
+    name: 'Coder',
     legs: 4,
     tails: 1,
     friends: ['Codeloccol Campers'],
    };
+   codeloccol.name='Happy coder'
    console.log(codeloccol)
